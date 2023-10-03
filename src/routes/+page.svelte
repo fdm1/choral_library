04a3638bcd1be7ae8460e729c0f9788f1ebf8e1f
Frank Massi <frankmassi@gmail.com>
2023-10-03 11:48:06 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Index page</title>
+</svelte:head>
+
+<h1>Index header</h1>
+
+<p>
+  Index page
+</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About page</h1>
+
+<p>
+  An about page
+</p>
